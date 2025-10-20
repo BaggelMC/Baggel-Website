3da1c2666735e1b2cd00e7cf96b89c90f0402499
Darkylt <darkprankingyt@gmail.com>
2025-10-21 01:20:01 +0200

    minimal changes

--- src/config.ts
+++ src/config.ts
@@ -2,3 +2,8 @@ export const LINKS = {
     discord: "https://discord.gg/p8wBMZXAfb",
     github: "https://github.com/BaggelMC"
 }
+
+export const RESOURCES = {
+    globalCSS: "/src/styles/global.css",
+    fontURL: "https://fonts.googleapis.com/css2?family=Cascadia+Code&display=swap"
+}
