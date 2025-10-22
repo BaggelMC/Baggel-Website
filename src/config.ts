2929e87dcebe42794e75c554675ccdfba52d317c
Darkylt <darkprankingyt@gmail.com>
2025-10-22 23:49:14 +0200

    Translating to german and more pages

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,11 @@
 export const LINKS = {
     discord: "https://discord.gg/p8wBMZXAfb",
-    github: "https://github.com/BaggelMC"
+    github: "https://github.com/BaggelMC",
+    email: "mail@baggel.de",
+    phone: "+49 123 4567890"
 }
 
 export const RESOURCES = {
     globalCSS: "/src/styles/global.css",
-    fontURL: "https://fonts.googleapis.com/css2?family=Cascadia+Code&display=swap"
+    fontURL: "https://fonts.googleapis.com/css2?family=Cascadia+Code&display=swap",
 }
