98d173ac7da275d028703f03e25f1b1b347064e6
darkyl <darkprankingyt@gmail.com>
2026-05-22 13:26:24 +0200

    Fix MiniMessage tool

---
+++ public/tools/minimessage/translation/data.mjs
@@ -0,0 +1,442 @@
+/**
+ * GENERATED FILE
+ * DO NOT MODIFY
+*/
+
+import dataAfZa from "./data/af_za.json" with { type: "json" };
+import dataArSa from "./data/ar_sa.json" with { type: "json" };
+import dataAstEs from "./data/ast_es.json" with { type: "json" };
+import dataAzAz from "./data/az_az.json" with { type: "json" };
+import dataBaRu from "./data/ba_ru.json" with { type: "json" };
+import dataBar from "./data/bar.json" with { type: "json" };
+import dataBeBy from "./data/be_by.json" with { type: "json" };
+import dataBeLatn from "./data/be_latn.json" with { type: "json" };
+import dataBgBg from "./data/bg_bg.json" with { type: "json" };
+import dataBrFr from "./data/br_fr.json" with { type: "json" };
+import dataBrb from "./data/brb.json" with { type: "json" };
+import dataBsBa from "./data/bs_ba.json" with { type: "json" };
+import dataCaEs from "./data/ca_es.json" with { type: "json" };
+import dataCsCz from "./data/cs_cz.json" with { type: "json" };
+import dataCvCu from "./data/cv_cu.json" with { type: "json" };
+import dataCyGb from "./data/cy_gb.json" with { type: "json" };
+import dataDaDk from "./data/da_dk.json" with { type: "json" };
+import dataDeAt from "./data/de_at.json" with { type: "json" };
+import dataDeCh from "./data/de_ch.json" with { type: "json" };
+import dataDeDe from "./data/de_de.json" with { type: "json" };
+import dataElGr from "./data/el_gr.json" with { type: "json" };
+import dataEnAu from "./data/en_au.json" with { type: "json" };
+import dataEnCa from "./data/en_ca.json" with { type: "json" };
+import dataEnGb from "./data/en_gb.json" with { type: "json" };
+import dataEnNz from "./data/en_nz.json" with { type: "json" };
+import dataEnPt from "./data/en_pt.json" with { type: "json" };
+import dataEnUd from "./data/en_ud.json" with { type: "json" };
+import dataEnUs from "./data/en_us.json" with { type: "json" };
+import dataEnp from "./data/enp.json" with { type: "json" };
+import dataEnws from "./data/enws.json" with { type: "json" };
+import dataEoUy from "./data/eo_uy.json" with { type: "json" };
+import dataEsAr from "./data/es_ar.json" with { type: "json" };
+import dataEsCl from "./data/es_cl.json" with { type: "json" };
+import dataEsEc from "./data/es_ec.json" with { type: "json" };
+import dataEsEs from "./data/es_es.json" with { type: "json" };
+import dataEsMx from "./data/es_mx.json" with { type: "json" };
+import dataEsUy from "./data/es_uy.json" with { type: "json" };
+import dataEsVe from "./data/es_ve.json" with { type: "json" };
+import dataEsan from "./data/esan.json" with { type: "json" };
+import dataEtEe from "./data/et_ee.json" with { type: "json" };
+import dataEuEs from "./data/eu_es.json" with { type: "json" };
+import dataFaIr from "./data/fa_ir.json" with { type: "json" };
+import dataFiFi from "./data/fi_fi.json" with { type: "json" };
+import dataFilPh from "./data/fil_ph.json" with { type: "json" };
+import dataFoFo from "./data/fo_fo.json" with { type: "json" };
+import dataFrCa from "./data/fr_ca.json" with { type: "json" };
+import dataFrCh from "./data/fr_ch.json" with { type: "json" };
+import dataFrFr from "./data/fr_fr.json" with { type: "json" };
+import dataFraDe from "./data/fra_de.json" with { type: "json" };
+import dataFurIt from "./data/fur_it.json" with { type: "json" };
+import dataFyNl from "./data/fy_nl.json" with { type: "json" };
+import dataGaIe from "./data/ga_ie.json" with { type: "json" };
+import dataGdGb from "./data/gd_gb.json" with { type: "json" };
+import dataGlEs from "./data/gl_es.json" with { type: "json" };
+import dataGoFr from "./data/go_fr.json" with { type: "json" };
+import dataHalUa from "./data/hal_ua.json" with { type: "json" };
+import dataHawUs from "./data/haw_us.json" with { type: "json" };
+import dataHeIl from "./data/he_il.json" with { type: "json" };
+import dataHiIn from "./data/hi_in.json" with { type: "json" };
+import dataHnNo from "./data/hn_no.json" with { type: "json" };
+import dataHrHr from "./data/hr_hr.json" with { type: "json" };
+import dataHuHu from "./data/hu_hu.json" with { type: "json" };
+import dataHyAm from "./data/hy_am.json" with { type: "json" };
+import dataIdId from "./data/id_id.json" with { type: "json" };
+import dataIgNg from "./data/ig_ng.json" with { type: "json" };
+import dataIoEn from "./data/io_en.json" with { type: "json" };
+import dataIsIs from "./data/is_is.json" with { type: "json" };
+import dataIsv from "./data/isv.json" with { type: "json" };
+import dataItIt from "./data/it_it.json" with { type: "json" };
+import dataJaJp from "./data/ja_jp.json" with { type: "json" };
+import dataJboEn from "./data/jbo_en.json" with { type: "json" };
+import dataKaGe from "./data/ka_ge.json" with { type: "json" };
+import dataKkKz from "./data/kk_kz.json" with { type: "json" };
+import dataKnIn from "./data/kn_in.json" with { type: "json" };
+import dataKoKr from "./data/ko_kr.json" with { type: "json" };
+import dataKsh from "./data/ksh.json" with { type: "json" };
+import dataKwGb from "./data/kw_gb.json" with { type: "json" };
+import dataKyKg from "./data/ky_kg.json" with { type: "json" };
+import dataLaLa from "./data/la_la.json" with { type: "json" };
+import dataLbLu from "./data/lb_lu.json" with { type: "json" };
+import dataLiLi from "./data/li_li.json" with { type: "json" };
+import dataLmo from "./data/lmo.json" with { type: "json" };
+import dataLoLa from "./data/lo_la.json" with { type: "json" };
+import dataLolUs from "./data/lol_us.json" with { type: "json" };
+import dataLtLt from "./data/lt_lt.json" with { type: "json" };
+import dataLvLv from "./data/lv_lv.json" with { type: "json" };
+import dataLzh from "./data/lzh.json" with { type: "json" };
+import dataMkMk from "./data/mk_mk.json" with { type: "json" };
+import dataMnMn from "./data/mn_mn.json" with { type: "json" };
+import dataMsMy from "./data/ms_my.json" with { type: "json" };
+import dataMtMt from "./data/mt_mt.json" with { type: "json" };
+import dataNah from "./data/nah.json" with { type: "json" };
+import dataNdsDe from "./data/nds_de.json" with { type: "json" };
+import dataNlBe from "./data/nl_be.json" with { type: "json" };
+import dataNlNl from "./data/nl_nl.json" with { type: "json" };
+import dataNnNo from "./data/nn_no.json" with { type: "json" };
+import dataNoNo from "./data/no_no.json" with { type: "json" };
+import dataOcFr from "./data/oc_fr.json" with { type: "json" };
+import dataOvd from "./data/ovd.json" with { type: "json" };
+import dataPlPl from "./data/pl_pl.json" with { type: "json" };
+import dataPls from "./data/pls.json" with { type: "json" };
+import dataPtBr from "./data/pt_br.json" with { type: "json" };
+import dataPtPt from "./data/pt_pt.json" with { type: "json" };
+import dataQcbEs from "./data/qcb_es.json" with { type: "json" };
+import dataQid from "./data/qid.json" with { type: "json" };
+import dataQyaAa from "./data/qya_aa.json" with { type: "json" };
+import dataRoRo from "./data/ro_ro.json" with { type: "json" };
+import dataRpr from "./data/rpr.json" with { type: "json" };
+import dataRuRu from "./data/ru_ru.json" with { type: "json" };
+import dataRyUa from "./data/ry_ua.json" with { type: "json" };
+import dataSahSah from "./data/sah_sah.json" with { type: "json" };
+import dataSeNo from "./data/se_no.json" with { type: "json" };
+import dataSkSk from "./data/sk_sk.json" with { type: "json" };
+import dataSlSi from "./data/sl_si.json" with { type: "json" };
+import dataSoSo from "./data/so_so.json" with { type: "json" };
+import dataSqAl from "./data/sq_al.json" with { type: "json" };
+import dataSrCs from "./data/sr_cs.json" with { type: "json" };
+import dataSrSp from "./data/sr_sp.json" with { type: "json" };
+import dataSvSe from "./data/sv_se.json" with { type: "json" };
+import dataSxu from "./data/sxu.json" with { type: "json" };
+import dataSzl from "./data/szl.json" with { type: "json" };
+import dataTaIn from "./data/ta_in.json" with { type: "json" };
+import dataThTh from "./data/th_th.json" with { type: "json" };
+import dataTlPh from "./data/tl_ph.json" with { type: "json" };
+import dataTlhAa from "./data/tlh_aa.json" with { type: "json" };
+import dataTok from "./data/tok.json" with { type: "json" };
+import dataTrTr from "./data/tr_tr.json" with { type: "json" };
+import dataTtRu from "./data/tt_ru.json" with { type: "json" };
+import dataTzoMx from "./data/tzo_mx.json" with { type: "json" };
+import dataUkUa from "./data/uk_ua.json" with { type: "json" };
+import dataUzUz from "./data/uz_uz.json" with { type: "json" };
+import dataValEs from "./data/val_es.json" with { type: "json" };
+import dataVecIt from "./data/vec_it.json" with { type: "json" };
+import dataViVn from "./data/vi_vn.json" with { type: "json" };
+import dataVpVl from "./data/vp_vl.json" with { type: "json" };
+import dataVro from "./data/vro.json" with { type: "json" };
+import dataYiDe from "./data/yi_de.json" with { type: "json" };
+import dataYoNg from "./data/yo_ng.json" with { type: "json" };
+import dataZhCn from "./data/zh_cn.json" with { type: "json" };
+import dataZhHk from "./data/zh_hk.json" with { type: "json" };
+import dataZhTw from "./data/zh_tw.json" with { type: "json" };
+import dataZlmArab from "./data/zlm_arab.json" with { type: "json" };
+
+const translations = Object.freeze({
+    "af_za": dataAfZa,
+    "ar_sa": dataArSa,
+    "ast_es": dataAstEs,
+    "az_az": dataAzAz,
+    "ba_ru": dataBaRu,
+    "bar": dataBar,
+    "be_by": dataBeBy,
+    "be_latn": dataBeLatn,
+    "bg_bg": dataBgBg,
+    "br_fr": dataBrFr,
+    "brb": dataBrb,
+    "bs_ba": dataBsBa,
+    "ca_es": dataCaEs,
+    "cs_cz": dataCsCz,
+    "cv_cu": dataCvCu,
+    "cy_gb": dataCyGb,
+    "da_dk": dataDaDk,
+    "de_at": dataDeAt,
+    "de_ch": dataDeCh,
+    "de_de": dataDeDe,
+    "el_gr": dataElGr,
+    "en_au": dataEnAu,
+    "en_ca": dataEnCa,
+    "en_gb": dataEnGb,
+    "en_nz": dataEnNz,
+    "en_pt": dataEnPt,
+    "en_ud": dataEnUd,
+    "en_us": dataEnUs,
+    "enp": dataEnp,
+    "enws": dataEnws,
+    "eo_uy": dataEoUy,
+    "es_ar": dataEsAr,
+    "es_cl": dataEsCl,
+    "es_ec": dataEsEc,
+    "es_es": dataEsEs,
+    "es_mx": dataEsMx,
+    "es_uy": dataEsUy,
+    "es_ve": dataEsVe,
+    "esan": dataEsan,
+    "et_ee": dataEtEe,
+    "eu_es": dataEuEs,
+    "fa_ir": dataFaIr,
+    "fi_fi": dataFiFi,
+    "fil_ph": dataFilPh,
+    "fo_fo": dataFoFo,
+    "fr_ca": dataFrCa,
+    "fr_ch": dataFrCh,
+    "fr_fr": dataFrFr,
+    "fra_de": dataFraDe,
+    "fur_it": dataFurIt,
+    "fy_nl": dataFyNl,
+    "ga_ie": dataGaIe,
+    "gd_gb": dataGdGb,
+    "gl_es": dataGlEs,
+    "go_fr": dataGoFr,
+    "hal_ua": dataHalUa,
+    "haw_us": dataHawUs,
+    "he_il": dataHeIl,
+    "hi_in": dataHiIn,
+    "hn_no": dataHnNo,
+    "hr_hr": dataHrHr,
+    "hu_hu": dataHuHu,
+    "hy_am": dataHyAm,
+    "id_id": dataIdId,
+    "ig_ng": dataIgNg,
+    "io_en": dataIoEn,
+    "is_is": dataIsIs,
+    "isv": dataIsv,
+    "it_it": dataItIt,
+    "ja_jp": dataJaJp,
+    "jbo_en": dataJboEn,
+    "ka_ge": dataKaGe,
+    "kk_kz": dataKkKz,
+    "kn_in": dataKnIn,
+    "ko_kr": dataKoKr,
+    "ksh": dataKsh,
+    "kw_gb": dataKwGb,
+    "ky_kg": dataKyKg,
+    "la_la": dataLaLa,
+    "lb_lu": dataLbLu,
+    "li_li": dataLiLi,
+    "lmo": dataLmo,
+    "lo_la": dataLoLa,
+    "lol_us": dataLolUs,
+    "lt_lt": dataLtLt,
+    "lv_lv": dataLvLv,
+    "lzh": dataLzh,
+    "mk_mk": dataMkMk,
+    "mn_mn": dataMnMn,
+    "ms_my": dataMsMy,
+    "mt_mt": dataMtMt,
+    "nah": dataNah,
+    "nds_de": dataNdsDe,
+    "nl_be": dataNlBe,
+    "nl_nl": dataNlNl,
+    "nn_no": dataNnNo,
+    "no_no": dataNoNo,
+    "oc_fr": dataOcFr,
+    "ovd": dataOvd,
+    "pl_pl": dataPlPl,
+    "pls": dataPls,
+    "pt_br": dataPtBr,
+    "pt_pt": dataPtPt,
+    "qcb_es": dataQcbEs,
+    "qid": dataQid,
+    "qya_aa": dataQyaAa,
+    "ro_ro": dataRoRo,
+    "rpr": dataRpr,
+    "ru_ru": dataRuRu,
+    "ry_ua": dataRyUa,
+    "sah_sah": dataSahSah,
+    "se_no": dataSeNo,
+    "sk_sk": dataSkSk,
+    "sl_si": dataSlSi,
+    "so_so": dataSoSo,
+    "sq_al": dataSqAl,
+    "sr_cs": dataSrCs,
+    "sr_sp": dataSrSp,
+    "sv_se": dataSvSe,
+    "sxu": dataSxu,
+    "szl": dataSzl,
+    "ta_in": dataTaIn,
+    "th_th": dataThTh,
+    "tl_ph": dataTlPh,
+    "tlh_aa": dataTlhAa,
+    "tok": dataTok,
+    "tr_tr": dataTrTr,
+    "tt_ru": dataTtRu,
+    "tzo_mx": dataTzoMx,
+    "uk_ua": dataUkUa,
+    "uz_uz": dataUzUz,
+    "val_es": dataValEs,
+    "vec_it": dataVecIt,
+    "vi_vn": dataViVn,
+    "vp_vl": dataVpVl,
+    "vro": dataVro,
+    "yi_de": dataYiDe,
+    "yo_ng": dataYoNg,
+    "zh_cn": dataZhCn,
+    "zh_hk": dataZhHk,
+    "zh_tw": dataZhTw,
+    "zlm_arab": dataZlmArab,
+});
+
+const locales = [
+    "af_za",
+    "ar_sa",
+    "ast_es",
+    "az_az",
+    "ba_ru",
+    "bar",
+    "be_by",
+    "be_latn",
+    "bg_bg",
+    "br_fr",
+    "brb",
+    "bs_ba",
+    "ca_es",
+    "cs_cz",
+    "cv_cu",
+    "cy_gb",
+    "da_dk",
+    "de_at",
+    "de_ch",
+    "de_de",
+    "el_gr",
+    "en_au",
+    "en_ca",
+    "en_gb",
+    "en_nz",
+    "en_pt",
+    "en_ud",
+    "en_us",
+    "enp",
+    "enws",
+    "eo_uy",
+    "es_ar",
+    "es_cl",
+    "es_ec",
+    "es_es",
+    "es_mx",
+    "es_uy",
+    "es_ve",
+    "esan",
+    "et_ee",
+    "eu_es",
+    "fa_ir",
+    "fi_fi",
+    "fil_ph",
+    "fo_fo",
+    "fr_ca",
+    "fr_ch",
+    "fr_fr",
+    "fra_de",
+    "fur_it",
+    "fy_nl",
+    "ga_ie",
+    "gd_gb",
+    "gl_es",
+    "go_fr",
+    "hal_ua",
+    "haw_us",
+    "he_il",
+    "hi_in",
+    "hn_no",
+    "hr_hr",
+    "hu_hu",
+    "hy_am",
+    "id_id",
+    "ig_ng",
+    "io_en",
+    "is_is",
+    "isv",
+    "it_it",
+    "ja_jp",
+    "jbo_en",
+    "ka_ge",
+    "kk_kz",
+    "kn_in",
+    "ko_kr",
+    "ksh",
+    "kw_gb",
+    "ky_kg",
+    "la_la",
+    "lb_lu",
+    "li_li",
+    "lmo",
+    "lo_la",
+    "lol_us",
+    "lt_lt",
+    "lv_lv",
+    "lzh",
+    "mk_mk",
+    "mn_mn",
+    "ms_my",
+    "mt_mt",
+    "nah",
+    "nds_de",
+    "nl_be",
+    "nl_nl",
+    "nn_no",
+    "no_no",
+    "oc_fr",
+    "ovd",
+    "pl_pl",
+    "pls",
+    "pt_br",
+    "pt_pt",
+    "qcb_es",
+    "qid",
+    "qya_aa",
+    "ro_ro",
+    "rpr",
+    "ru_ru",
+    "ry_ua",
+    "sah_sah",
+    "se_no",
+    "sk_sk",
+    "sl_si",
+    "so_so",
+    "sq_al",
+    "sr_cs",
+    "sr_sp",
+    "sv_se",
+    "sxu",
+    "szl",
+    "ta_in",
+    "th_th",
+    "tl_ph",
+    "tlh_aa",
+    "tok",
+    "tr_tr",
+    "tt_ru",
+    "tzo_mx",
+    "uk_ua",
+    "uz_uz",
+    "val_es",
+    "vec_it",
+    "vi_vn",
+    "vp_vl",
+    "vro",
+    "yi_de",
+    "yo_ng",
+    "zh_cn",
+    "zh_hk",
+    "zh_tw",
+    "zlm_arab",
+];
+
+const version = "26.1.2";
+
+const data = { translations, locales, version };
+export default data;
