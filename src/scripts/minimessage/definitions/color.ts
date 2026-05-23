3fc399cca44c4a53652f5c4658a3cbd586add405
darkyl <darkprankingyt@gmail.com>
2026-05-23 16:01:16 +0200

    Hover tool

--- src/scripts/minimessage/definitions/color.ts
+++ src/scripts/minimessage/definitions/color.ts
@@ -27,7 +27,29 @@ const presetRows: string[][] = [
   ["black", "dark_blue", "dark_green", "dark_aqua", "dark_red", "dark_purple", "gold", "gray"]
 ];
 
-const toolbarIcon = `<svg  viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><defs><style>.cls-1{fill:none;stroke:currentColor;stroke-miterlimit:10;stroke-width:1.92px;}</style></defs><g id="roll_brush" data-name="roll brush"><circle class="cls-1" cx="5.73" cy="13.45" r="0.48"/><circle class="cls-1" cx="7.65" cy="18.24" r="0.48"/><circle class="cls-1" cx="6.69" cy="8.65" r="0.48"/><circle class="cls-1" cx="10.52" cy="5.78" r="0.48"/><circle class="cls-1" cx="15.32" cy="6.74" r="0.48"/><circle class="cls-1" cx="18.2" cy="10.57" r="0.48"/><path class="cls-1" d="M22.51,11.86a4.87,4.87,0,0,1-4.86,4.95H16.18a4.28,4.28,0,0,0-3.57,1.91l-1.15,1.72a4.74,4.74,0,0,1-4,2.12h0a4.61,4.61,0,0,1-3.87-2A13.07,13.07,0,0,1,1.41,13.3V12a10.55,10.55,0,0,1,21.1-.15Z"/></g></svg>`;
+const toolbarIcon = `<svg fill="currentColor" width="800px" height="800px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+
+<g data-name="Layer 2">
+
+<g data-name="color-palette">
+
+<rect width="24" height="24" opacity="0"/>
+
+<path d="M19.54 5.08A10.61 10.61 0 0 0 11.91 2a10 10 0 0 0-.05 20 2.58 2.58 0 0 0 2.53-1.89 2.52 2.52 0 0 0-.57-2.28.5.5 0 0 1 .37-.83h1.65A6.15 6.15 0 0 0 22 11.33a8.48 8.48 0 0 0-2.46-6.25zM15.88 15h-1.65a2.49 2.49 0 0 0-1.87 4.15.49.49 0 0 1 .12.49c-.05.21-.28.34-.59.36a8 8 0 0 1-7.82-9.11A8.1 8.1 0 0 1 11.92 4H12a8.47 8.47 0 0 1 6.1 2.48 6.5 6.5 0 0 1 1.9 4.77A4.17 4.17 0 0 1 15.88 15z"/>
+
+<circle cx="12" cy="6.5" r="1.5"/>
+
+<path d="M15.25 7.2a1.5 1.5 0 1 0 2.05.55 1.5 1.5 0 0 0-2.05-.55z"/>
+
+<path d="M8.75 7.2a1.5 1.5 0 1 0 .55 2.05 1.5 1.5 0 0 0-.55-2.05z"/>
+
+<path d="M6.16 11.26a1.5 1.5 0 1 0 2.08.4 1.49 1.49 0 0 0-2.08-.4z"/>
+
+</g>
+
+</g>
+
+</svg>`;
 
 export const colorTag: TagDefinition = {
   id: "color",
